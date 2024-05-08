da5d34e6356418fb40ed4d0fc2da8e95e39826d6
wjdals95 <wjdals992@naver.com>
2024-05-08 12:09:44 +0900

    build

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -5,8 +5,8 @@ import styles from './CommonSearchBar.module.scss'
 import { pageState } from '@/recoil/atoms/pageState'
 
 function CommonSearchBar() {
-   const [search, setSearch] = useRecoilState(searchState)
-   const [page, setPage] = useRecoilState(pageState)
+   const [, setSearch] = useRecoilState(searchState)
+   const [, setPage] = useRecoilState(pageState)
    const [text, setText] = useState('')
 
    const onChange = event => {
